27156df460662e48e93973fcfd4cc48e79cb1104
KalsonSaint <saintkalson@gmail.com>
2020-08-05 23:22:24 +0100

    Working with Routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
+
 import './App.css';
 
 import  HomePage  from './pages/homepage/homepage'
+
+const HatsPage = () => (
+  <>
+  <h1>Hats Pages</h1>
+  </>
+);
 
 function App() {
   return (
