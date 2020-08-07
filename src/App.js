8e347bc291d279384571a3b84f30231add015ee3
KalsonSaint <saintkalson@gmail.com>
2020-08-07 17:52:38 +0100

    Form Components Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import  HomePage  from './pages/homepage/homepage'
-import  ShopPage  from './pages/shop/shop'
-import Header from './components/header/Header'
+import HomePage from "./pages/homepage/Homepage";
+import ShopPage from "./pages/shop/Shop";
+import LoginRegister from "./pages/login-register/login-register";
+import Header from "./components/header/Header";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/login" component={LoginRegister} />
       </Switch>
     </div>
   );
---
+++ src/pages/login-register/login-register.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './login-register.scss'
+
+import Login from '../../components/login/login'
+
+const LoginRegister = () => {
+    return (
+        <div className="login-register">
+            <Login />
+        </div>
+    )
+}
+
+export default LoginRegister 
