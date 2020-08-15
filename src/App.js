cfc07c6167f6b48397bbcbced5f1dee09680766a
KalsonSaint <saintkalson@gmail.com>
2020-08-15 23:50:15 +0100

    SignUp inProgress

--- src/App.js
+++ src/App.js
@@ -14,31 +14,39 @@ class App extends Component {
     super();
 
     this.state = {
-      currentUser : null
-    }
+      currentUser: null,
+    };
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot(snapShot => {
-          console.log(snapShot.data())
-        })
+  
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
       }
+      this.setState({ currentUser: userAuth });
     });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/pages/login-register/login-register.js
+++ src/pages/login-register/login-register.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import './login-register.scss'
-import Login from '../../components/login/login'
+import React from "react";
+import "./login-register.scss";
+
+import Login from "../../components/login/login";
+import SignUp from "../../components/sign-up/SignUp";
 
 const LoginRegister = () => {
-    return (
-        <div className="login-register">
-            <Login />
-        </div>
-    )
+  return (
+    <div className="login-register">
+      <Login />
+      <SignUp />
+    </div>
+  );
 };
 
-export default LoginRegister 
+export default LoginRegister;
